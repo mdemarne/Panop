3ec0b3313c1c1926d555ed27fbfea447bdb0dfe7
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-08-30 14:48:07 +0200

    Moving to Scala 2.11.7

--- project/Build.scala
+++ project/Build.scala
@@ -49,25 +49,11 @@ object PanopBuild extends Build {
       description := "Simple Tool For Parallel Online Search",
       organization := "name.demarne.m",
       version := "1.0-SNAPSHOT",
-      scalaVersion := "2.11.6",
+      scalaVersion := "2.11.7",
       libraryDependencies ++= Seq(
         "com.typesafe.akka" %% "akka-actor" % "2.3.12",
         "org.scalaj" %% "scalaj-http" % "1.1.5",
         "org.scalatest" %% "scalatest" % "2.2.4" % "test",
         "org.scala-lang" % "scala-compiler" % "2.11.6")
       ) ++ Seq(cleanTask, scriptTask))
-
-  /*lazy val root = (project in file(".")).
-    settings(
-      name := "panop",
-      description := "Simple Tool For Parallel Online Search",
-      organization := "name.demarne.m",
-      scalaVersion := "2.11.6",
-      libraryDependencies ++= Seq(
-        "com.typesafe.akka" %% "akka-actor" % "2.3.12",
-        "org.scalaj" %% "scalaj-http" % "1.1.5",
-        "org.scalatest" %% "scalatest" % "2.2.4" % "test",
-        "org.scala-lang" % "scala-compiler" % "2.11.6"),
-      scalacOptions ++= Seq("-feature", "-deprecation", "-unchecked"))*/
-
 }
