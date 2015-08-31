8c48a0b2b47734794794629990223b7e44251a35
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-08-31 10:02:04 +0200

    BF: fixing gitignore

---
+++ src/main/scala/panop/Settings.scala
@@ -0,0 +1,20 @@
+package panop
+
+import scala.util.matching.Regex
+import scala.util.parsing.combinator.RegexParsers
+
+/**
+ * Setting object containig default parameters.
+ * @author Mathieu Demarne (mathieu.demarne@gmail.com)
+ */
+object Settings {
+  import com._
+  // TODO: in the future, this could be moved to Akka configuration for instance.
+  val defMode = BFSMode
+  val defIgnExts = "js|css|pdf|png|jpg|gif|jpeg|svg|tiff".r
+  val defTopBnds = "<body>|<BODY>".r
+  val defBotBnds = "</body>|</BODY>".r
+  val defMaxSlaves = 200
+  val defSlaves = 20
+  val defMaxCoTentatives = 10
+}
